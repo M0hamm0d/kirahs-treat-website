91967bbaa5894707614fa2176a29a49479482526
Muhammad Abdussalam <masalami042@gmail.com>
2026-03-12 11:51:13 +0100

    fix: update alias resolution in Vite config to use fileURLToPath

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      // This tells Vite that @/ is a shortcut for the src folder
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
